bf53bdcddedfa61e985f92abfc5e396410ccc1fb
Jory Tindall <jory@hypecreativestudios.com>
2021-06-23 11:14:11 -0700

    Removed blockquote

--- src/components/richText/Serializers.js
+++ src/components/richText/Serializers.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { H1, H2, H3, H4, H5, H6, Paragraph } from "../typography"
 import ResolvedLink from "./ResolvedLink"
-import { Blockquote } from "../blockquote"
+import { Blockquote } from '../blockquote'
 
 const Serializers = {
     types: {
